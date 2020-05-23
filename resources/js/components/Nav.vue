ae259385d5ee01d10a54bb3cffee583bffaebbb8
CheezeSlayer <hchang1994@hotmail.com>
2020-05-23 14:19:32 -0700

    set auth user, implemented vuex for set title and auth user

--- resources/js/components/Nav.vue
+++ resources/js/components/Nav.vue
@@ -32,7 +32,7 @@
                     .9-.3 1.1-.7.3-.5.2-1-.2-1.3zm-2.5-8h-4.3l5 4.5V3.6c0-.3-.3-.6-.7-.6z"/>
                 </svg>
             </router-link>
-            <router-link to="/" class="px-6 border-b-2 border-white h-full flex items-center">
+            <router-link :to="'/users/' + authUser.data.user_id" class="px-6 border-b-2 border-white h-full flex items-center">
                 <img :src="'../storage/img/profile.JPG'" alt="profile image for user" class="w-8 h-8 object-cover rounded-full">
             </router-link>
             <router-link to="/" class="px-6 border-b-2 border-white h-full flex items-center">
@@ -62,8 +62,16 @@
 </template>
 
 <script>
+    import { mapGetters } from 'vuex';
+
     export default {
-        
+        name: "Nav",
+
+        computed: {
+            ...mapGetters({
+                authUser: 'authUser'
+            })
+        }
     }
 </script>
 
